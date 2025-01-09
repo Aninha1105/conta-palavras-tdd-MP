641a95b447a18bb83442fc0ac40c59c7621f9fa9
Aninha1105 <analuparaguassu01@gmail.com>
2025-01-09 13:11:10 -0300

    style: formatação do código com cpplint

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -4,7 +4,10 @@
  * \author Ana Luísa de Souza Paraguassu - 231003442.
  */
 #include "conta_palavras.hpp"
+#include <vector>
+#include <utility>
+#include <string>
 
-std::vector<std::pair<std::string,int>> ContaPalavras (const std::string& texto){
-   return {};
+std::vector<std::pair<std::string, int>> ContaPalavras(const std::string& texto) {
+  return {};
 }
--- conta_palavras.hpp
+++ conta_palavras.hpp
@@ -4,12 +4,13 @@
  * \author Ana Luísa de Souza Paraguassu - 231003442.
  */
 
-#ifndef CONTA_PALAVRAS_HPP
-#define CONTA_PALAVRAS_HPP
+#ifndef CONTA_PALAVRAS_HPP_
+#define CONTA_PALAVRAS_HPP_
 
 #include <string>
 #include <vector>
+#include <utility>
 
-std::vector<std::pair<std::string,int>> ContaPalavras (const std::string& texto) ;
+std::vector<std::pair<std::string, int>> ContaPalavras(const std::string& texto);
 
-#endif  // CONTA_PALAVRAS_HPP
+#endif  // CONTA_PALAVRAS_HPP_
