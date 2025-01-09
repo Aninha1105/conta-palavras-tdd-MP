50119152892ef1edc3542bafedd250788bba6dca
Aninha1105 <analuparaguassu01@gmail.com>
2025-01-09 16:54:12 -0300

    fix: implementação da função separarEContar() test: Teste 2 (passou)

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -3,17 +3,35 @@
  * \brief Implementação das funções para contagem e ordenação das palavras do texto.
  * \author Ana Luísa de Souza Paraguassu - 231003442.
  */
+
 #include "conta_palavras.hpp"
 #include <vector>
 #include <utility>
 #include <string>
 
 void separarEContar(const std::string& texto, std::vector<std::pair<std::string,int>>& palavras){
- return;
+ std::string palavra_atual = "";
+ for(char c : texto){
+  if(c == ' ' || c == '\n'){
+    if(!palavra_atual.empty()){
+      palavras.push_back({palavra_atual,1}); 
+      palavra_atual = "" ;
+    }
+  } else{
+    palavra_atual+=c;
+  }
+ }
+
+ if(!palavra_atual.empty()){
+  palavras.push_back({palavra_atual,1});
+ }
+
 }
 
 std::vector<std::pair<std::string, int>> ContaPalavras(const std::string& texto) {
   std::vector<std::pair<std::string,int>> palavras;
+
    separarEContar(texto, palavras);
-  return {};
+   
+  return palavras;
 }
