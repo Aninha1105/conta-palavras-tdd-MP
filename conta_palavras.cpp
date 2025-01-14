4ce0d63afb103faf8a6c718378991afa71dec850
Aninha1105 <analuparaguassu01@gmail.com>
2025-01-13 21:39:34 -0300

    feat: criação da função principal ContaPalavras()

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -3,3 +3,6 @@
  * \brief Implementação das funções para contagem e ordenação das palavras do texto.
  * \author Ana Luísa de Souza Paraguassu - 231003442.
  */
+#include "conta_palavras.hpp"
+
+void ContaPalavras(std::string texto){}
--- conta_palavras.hpp
+++ conta_palavras.hpp
@@ -3,3 +3,7 @@
  * \brief Declaração das funções para o contador de palavras.
  * \author Ana Luísa de Souza Paraguassu - 231003442.
  */
+
+#include <string>
+
+void ContaPalavras(std::string texto);
