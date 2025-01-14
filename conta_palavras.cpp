26132ee317fdaf862fc067bb40c645a765a550db
Aninha1105 <analuparaguassu01@gmail.com>
2025-01-14 00:17:30 -0300

    fix: implementação da funcionalidade de contar ocorrências das palavras na função separarEContar() test: Teste 4 (passou)

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -24,11 +24,20 @@ std::string lerArquivo(std::string nome_do_arquivo) {
 std::vector<std::pair<std::string, int>> separarEContar(std::string texto) {
   std::vector<std::pair<std::string, int>> dicionario;
   std::string palavra_atual = "";
-
-  for (char c : texto) {
-    if (c == ' ' || c == '\n') {
-      if (!palavra_atual.empty()) {
-        dicionario.push_back({palavra_atual, 1});
+  for(char c : texto){
+    if(c == ' ' || c == '\n'){
+      if(!palavra_atual.empty()){
+        bool encontrada = false;
+        for (auto& par : dicionario) {
+          if (par.first == palavra_atual) {
+            par.second++;
+            encontrada = true;
+            break;
+          }
+        }
+        if(!encontrada){
+          dicionario.push_back({palavra_atual,1});
+        }
         palavra_atual = "";
       }
     } else {
@@ -36,8 +45,18 @@ std::vector<std::pair<std::string, int>> separarEContar(std::string texto) {
     }
   }
 
-  if (!palavra_atual.empty()) {
-    dicionario.push_back({palavra_atual, 1});
+  if(!palavra_atual.empty()){
+    bool encontrada = false;
+    for(auto& par : dicionario){
+      if(par.first == palavra_atual){
+        par.second++;
+        encontrada = true;
+        break;
+      }
+    }
+    if (!encontrada){
+      dicionario.push_back({palavra_atual,1});
+    }
   }
 
   return dicionario;
